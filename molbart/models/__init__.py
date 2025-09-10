011e8fb837a9f53f4ac22b8fe2c919a4b97eb82b
newbee1905 <beenewminh@outlook.com>
2025-09-10 20:00:03 +1000

    fix: fix cycle import

--- molbart/models/__init__.py
+++ molbart/models/__init__.py
@@ -1,3 +1,3 @@
 from .base_transformer import _AbsTransformerModel
-from .transformer_models import BARTModel, UnifiedModel, CycleConsistencyBARTModel
+from .transformer_models import BARTModel, UnifiedModel, CycleConsistencyBARTModel, DifferentiableCycleBARTModel
 from .chemformer import Chemformer
