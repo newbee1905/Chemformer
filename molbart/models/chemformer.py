1d34c69dfe2345fe70e384bfbba760c488d6a32e
newbee1905 <beenewminh@outlook.com>
2025-09-17 08:18:10 +1000

    fix: typo

--- molbart/models/chemformer.py
+++ molbart/models/chemformer.py
@@ -317,7 +317,7 @@ class Chemformer:
                 warm_up_steps=args.get("warm_up_steps"),
                 **extra_args,
                 w_retro=args.get("w_retro", 1.0), 
-                w_cos=args.get("w_cos", 0.1)
+                w_cos=args.get("w_cos", 0.1),
                 gumbel_tau=args.get("gumbel_tau", 1.0),
             )
         elif self.model_type == "diff_cycle_bart":
